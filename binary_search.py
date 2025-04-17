e84e3432a90638324daab87b6a4d881ac971a4e2
Mamta Jangra <mamtajangra204@gmail.com>
2025-04-17 18:11:31 +0530

    Refactor binary search function and update example usage

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,16 @@
 def binary_search(arr,val_find):
-  low =0
-  high = len(arr)
-  while low < high:
-    mid = (low+high)//2
-    if val_find == arr[mid]:
-      return mid
-    if val_find < arr[mid]:
-      high = mid-1
-    else:
-      low = mid+1
-    return "nothing"  
+   low =0
+   high = len(arr)
+   while low < high:
+        mid = (low+high)//2
+        if val_find == arr[mid]:
+            return mid
+        if val_find < arr[mid]:
+            high = mid-1
+        else:
+            low = mid+1 
 
  
-array = [1, 2, 5, 12, 22, 33, 98]
-value = 22
-binary_search(array,value)
+array = [1, 2, 5, 12, 19, 33, 98]
+value = 33
+print(binary_search(array,value))
