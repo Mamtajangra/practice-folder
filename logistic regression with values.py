d2e52a35c1b281fc812a6ce970fa4d98f80879af
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-01 18:36:01 +0530

    Fix input data and adjust range for logistic regression model

--- logistic regression with values.py
+++ logistic regression with values.py
@@ -5,8 +5,8 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 
 # give input and classification of pass fail
-X = np.array([[100],[63],[40],[85]])
-y = np.array([1,0,1,0])
+X = np.array([[100],[80],[60],[85]]).reshape(-1,1)
+y = np.array([1,0,0,1])
 X_new = np.array([[150]])
 
 
@@ -15,14 +15,15 @@ model = LogisticRegression()
 # provide X and y to model 
 model.fit(X,y)
 
-# it predicts the value according to x 
+# it predicts the value according to x_new 
 y_predict = model.predict(X_new)[0]
-# we also want to know probability 
+# we also want to know probability which should in between 0 and 1
 probability = model.predict_proba(X_new)[0][1]
 
-
-X_range = np.linspace(0,160,300).reshape(-1,1)
-y_proba = model.predict(X_range)[:,1]
+# here we will plot several points 0 to 120  in 2d format 
+X_range = np.linspace(0,120,300).reshape(-1,1)
+# now we want probability of pass 
+y_proba = model.predict_proba(X_range)[: , 1]
 
 
 # pltting the sigmoid function
@@ -31,7 +32,7 @@ plt.scatter(X_new, probability, label="Actual Data")
 plt.scatter(X, y, color='blue', label="Training Data")
 plt.plot(X_range, y_proba, color='red', label="sigmoid curve")
 plt.xlabel("Mark")
-plt.ylabel("result")
+plt.ylabel("result" )                   ## 0= fail,1= pass)
 plt.title("Logistic Regression: Original Scale")
 plt.legend()
 plt.grid(True)
